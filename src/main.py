3eeb8fac40d8bc779d5d73db52acb845e17556f2
anderiitur73 <anderiitur@gmail.com>
2026-04-05 19:37:09 +0200

    metrics file created and works properly with: daily returns, volatility,sharpe and max drop

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 from data_loader import get_data
+from metrics import daily_returns,cumulative_returns,volatility,sharpe_ratio,max_drawdown
 
 tickers = ["SPY", "AAPL", "MSFT"]
 data = get_data(tickers)
+returns=daily_returns(data)
+cum_ret=cumulative_returns(returns)
 
 print(data.head())      # primeras 5 filas
 print(data.shape)       # cuántas filas × columnas tienes
 print(data.dtypes)      # tipo de dato de cada columna
+
+print("=== Acumulated rent (last five days) ===")
+print(cum_ret.tail())
+
+print("\n=== Anual volatility ===")
+print(volatility(returns).round(4))
+
+print("\n=== Sharpe Ratio ===")
+print(sharpe_ratio(returns).round(4))
+
+print("\n=== Max Drawdown ===")
+print(max_drawdown(cum_ret).round(4))
---
+++ src/metrics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+def daily_returns(data):
+    return data.pct_change().dropna()
+    
+def cumulative_returns(returns):
+    return (1+returns).cumprod()
+
+def volatility(returns):
+    return returns.std()*np.sqrt(252)
+
+def sharpe_ratio(returns,risk_free=0.04):
+    excess_returns=returns-risk_free/252
+    return (excess_returns.mean() / returns.std()) * np.sqrt(252)
+
+def max_drawdown(cum_returns):
+    peak = cum_returns.cummax()
+    drawdown = (cum_returns - peak) / peak
+    return drawdown.min()
